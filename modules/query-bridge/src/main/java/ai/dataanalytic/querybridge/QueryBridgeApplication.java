f5804ab2dc5f98f4fbfdcca32f72bfb863d9b2c5
Jocsan Mairena <jocsan.mairena@gmail.com>
2024-09-24 20:57:18 -0400

    Rename modules and main class for DataBridge application
    Updated the run configuration module name to "data-bridge" and main class to "DataBridgeApplication". Commented out the old "QueryBridgeApplication" class to reflect the new naming convention in the project structure.

--- modules/query-bridge/src/main/java/ai/dataanalytic/querybridge/QueryBridgeApplication.java
+++ modules/query-bridge/src/main/java/ai/dataanalytic/querybridge/QueryBridgeApplication.java
@@ -1,13 +1,13 @@
-package ai.dataanalytic.querybridge;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication()
-public class QueryBridgeApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(QueryBridgeApplication.class, args);
-    }
-
-}
+//package ai.dataanalytic.querybridge;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication()
+//public class QueryBridgeApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(QueryBridgeApplication.class, args);
+//    }
+//
+//}
