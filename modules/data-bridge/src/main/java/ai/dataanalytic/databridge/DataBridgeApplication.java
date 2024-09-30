90815acf514cea79e2e06da5efd45e7895bf1c46
Enoc.Velza(Fungicoder) <fungicoder@gmail.com>
2024-09-30 13:29:24 -0600

    uncomment DataBridgeApplication.java main class

--- modules/data-bridge/src/main/java/ai/dataanalytic/databridge/DataBridgeApplication.java
+++ modules/data-bridge/src/main/java/ai/dataanalytic/databridge/DataBridgeApplication.java
@@ -1,14 +1,14 @@
-//package ai.dataanalytic.databridge;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.ComponentScan;
-//
-//@SpringBootApplication(scanBasePackages = "ai.dataanalytic.databridge")
-//public class DataBridgeApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(DataBridgeApplication.class, args);
-//    }
-//
-//}
+package ai.dataanalytic.databridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(scanBasePackages = "ai.dataanalytic.databridge")
+public class DataBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataBridgeApplication.class, args);
+    }
+
+}
